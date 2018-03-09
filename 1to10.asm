0d0b5fda55b7c20d5cc14549c018abe7fe8aa621
rudyerudite <31957629+rudyerudite@users.noreply.github.com>
2018-03-09 08:26:02 +0530

    Add files via upload

---
+++ 1to10.asm
@@ -0,0 +1,28 @@
+extern printf
+
+section .data
+	out:dd "%d",0
+
+section .text
+	global main
+
+	main:
+	push ebp
+	mov ebp,esp
+	mov ebx,0
+
+	
+
+	l1:
+	inc ebx
+	push ebx
+	push out
+	call printf
+	cmp ebx,10
+	jne l1
+	
+	
+	
+	leave
+	ret
+	
