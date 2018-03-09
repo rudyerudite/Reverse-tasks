6329327eb1eb217968f055e4c3efe3ea9178b171
rudyerudite <31957629+rudyerudite@users.noreply.github.com>
2018-03-09 19:03:37 +0530

    Add files via upload

---
+++ greet.asm
@@ -0,0 +1,35 @@
+extern printf
+extern scanf
+
+section .data
+	msg:db "Enter your name here:",10,0
+	in:db "%s",0
+
+	name: db 0
+	
+	msg1: db "Hey %s! Hope you liked my code!",10,0
+	
+section .text
+	global main
+
+	main:
+	push ebp
+	mov ebp,esp
+
+	push msg
+	call printf
+
+	
+	lea eax,[ebx+0x4]
+	push in
+	call scanf
+
+	pop eax
+	push msg1
+	call printf
+
+	leave
+	ret
+
+
+
