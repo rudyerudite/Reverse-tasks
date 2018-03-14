fc89c51557cea952cca00fde4329b995e19f68f5
Shruti Dixit <31957629+rudyerudite@users.noreply.github.com>
2018-03-14 18:17:33 +0530

    Add files via upload

---
+++ fib.asm
@@ -0,0 +1,55 @@
+extern scanf
+extern printf
+
+
+section .data
+		n dd 0
+		out: dd "%d"
+
+
+section .text
+		global main
+
+		main:
+
+		push ebp
+		mov ebp,esp
+
+		push n
+		push out
+		call scanf
+
+		mov eax,0
+		mov ebx,0
+		mov ecx,1
+		mov edx,2
+
+		fib:
+
+		mov ebx,eax
+		add ebx,ecx
+
+		mov eax,ecx
+		mov ecx,ebx
+		inc edx
+		cmp edx,[n]
+		jne fib
+
+		push ebx
+		push out
+		call printf
+
+		leave
+		ret
+
+
+
+
+
+
+		
+
+
+
+
+
