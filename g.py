f139a1e3a1cad11933971f5f0fdf6534489c5645
Shruti Dixit <31957629+rudyerudite@users.noreply.github.com>
2018-03-20 23:59:54 +0530

    Add files via upload

--- g.py
+++ g.py
@@ -2,13 +2,14 @@ from z3 import *
 
 x=Real('x')
 y=Real('y')
-z=Real('z')
 
+a=int(input())
+b=int(input())
+c=int(input())
 s=Solver()
 
-s.add(3*x+2*y-z==1)
-s.add(2*x-2*y+4*z==-2)
-s.add(-x+0.5*y-z==0)
-
+s.add(b==x+y)
+s.add(c==x*y)
 print(s.check())
 print(s.model())
+
