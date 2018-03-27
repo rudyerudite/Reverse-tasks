a0f05f2bacd2358c9b31f5c72ab40abaaa2d82d8
Shruti Dixit <31957629+rudyerudite@users.noreply.github.com>
2018-03-27 08:58:41 +0530

    Add files via upload

--- g.py
+++ g.py
@@ -1,15 +1,28 @@
 from z3 import *
 
-x=Real('x')
-y=Real('y')
+x=BitVec('x',35)
+y=BitVec('y',35)
+s1=BitVec('s1',35)
+s2=BitVec('s2',35)
+s1=input()
+s2=input()
 
-a=int(input())
-b=int(input())
-c=int(input())
 s=Solver()
 
-s.add(b==x+y)
-s.add(c==x*y)
-print(s.check())
-print(s.model())
+p=4646704883
 
+a=(2*x+3)%p
+b=(3*y+9)%p
+
+s.add(s1==a^b)
+s.add(s2==((2*a+3)%p)^((3*b+9)%p))
+s.add(x>=0)
+s.add(y>=0)
+s.add(x<4646704883)
+s.add(y<4646704883)
+
+while(s.check()==sat):
+	m=s.model()
+	print(m[x])
+	print(m[y])
+	s.add(Or(x!=m[x],y!=m[y]))
