0fae3aab8a093e00e558e2658e97350d1ed48907
Shruti Dixit <31957629+rudyerudite@users.noreply.github.com>
2021-03-06 11:21:20 +0530

    Update barbie-session.py

--- barbie-session.py
+++ barbie-session.py
@@ -1,18 +1,12 @@
 from z3 import * 
 
-#check Sudoku, distinct
-""" Sudoku board example
-_ _ _ _ 1 _ _ 3 _
-_ _ 9 _ _ 5 _ _ 8
-8 _ 4 _ _ 6 _ 2 5
-_ _ _ _ _ _ 6 _ _
-_ _ 8 _ _ 4 _ _ _
-1 2 _ _ 8 7 _ _ _
-3 _ _ 9 _ _ 2 _ _
-_ 6 5 _ _ 8 _ _ _
-9 _ _ _ _ _ _ _ _
-"""
-
+'''
+add eax ecx
+shl eax
+mul ecx
+mov edx eax
+xor edx eax
+'''
 
 s = z3.Solver()
 
@@ -21,7 +15,7 @@ ecx = z3.BitVec('ecx', 32)
 eax1, eax2, eax3 = z3.BitVecs('eax1 eax2 eax3',32)
 
 edx1,edx2 = z3.BitVecs('edx1 edx2',32)
-model = [eax1==eax+ecx,eax2==eax1<<z3.BitVec(12,32), eax3==eax2*ecx, edx1==eax3,edx2==edx1*eax]
+model = [eax1==eax+ecx,eax2==eax1<<z3.BitVec(12,32), eax3==eax2*ecx, edx1==eax3,edx2==edx1^eax]
 print(model)
 s.add(model)
 print(s.check())
